969b47d0c29e45c357a3b16e4c14f4e0d3c3db0f
CastilloAbel <castillokev.abel@gmail.com>
2023-11-11 13:32:00 -0300

    ventana principal

--- Interfaces/main.py
+++ Interfaces/main.py
@@ -1,6 +1,2 @@
-from VentanaLibros.ListadoLibros import ListadoLibros
-from VentanaPrestamos.ListadoPrestamos import ListadoPrestamos
-from VentanaSocios.ListadoSocios import ListadoSocios
-ListadoLibros().mostrar()
-ListadoPrestamos().mostrar()
-ListadoSocios().mostrar()
+from VentanaPrincipal import VentanaPrincipal
+VentanaPrincipal().mostrar()
