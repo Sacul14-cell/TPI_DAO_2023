68ec18cad529db37a2cbe162247a66737eb4e090
Lucas Ivan Montes <65433774+Sacul14-cell@users.noreply.github.com>
2023-11-15 19:13:58 -0300

    Se cambio la iteración

--- Biblioteca18/Entidades/iteradores/iteradorPrestamosDemorados.py
+++ Biblioteca18/Entidades/iteradores/iteradorPrestamosDemorados.py
@@ -1,12 +1,12 @@
-class iteradorPrestamosDemorados:
+class IteradorPrestamosDemorados:
     def __int__(self, listaPrestamos:list):
         self.listaPrestamos = listaPrestamos
 
     def listaPrestamosDemorados(self):
-        listaDemoras = []
         for prestamo in self.listaPrestamos:
             demora= prestamo.calcularDemora()
             if demora > 0:
-                linea = "El libro " + str(prestamo.libro.get_titulo()) + "tiene una demora de " + str(demora) + "dias"
-                listaDemoras.append(linea)
+                #linea = "El libro " + str(prestamo.libro.get_titulo()) + "tiene una demora de " + str(demora) + "dias"
+                dato=[str(prestamo.libro.get_titulo()),str(demora)]
+                listaDemoras.append(dato)
             return listaDemoras
